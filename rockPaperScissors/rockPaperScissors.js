92460e78e4277243819c1de3aec93ec8653d8ac8
nissaw <nissa.wollum@gmail.com>
2015-10-14 09:22:22 -0700

    RPS

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -17,7 +17,58 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
-  // TODO: your solution here
+var rockPaperScissors = function (rounds) {
+  var collections = [];
+  var plays = ['rock', 'paper', 'scissors'];
+
+
+  var makeTree = function (play) {
+    var tree =  {};
+    tree.value = play;
+    tree.children = [];
+    _.extend(tree, treeMethods);
+    return tree;
+  };
+
+    var treeMethods = {};
+    treeMethods.addRound = function() {
+      var plays = ['rock', 'paper', 'scissors'];
+    
+      
+      if ( this.children === undefined ) { // has no current children add directly to its array
+        for (var i = 0; i < plays.length; i++ ) {
+          this.children.push(makeTree(plays[i]));
+        }
+      } else { // already has children iterate through and add args to all of its children
+        for (var j = 0; j < this.children.length; j++ ) { // for each child
+            for (var k = 0; k < plays.length; k++ )
+          this.children[j].children.push(makeTree(plays[k]));  // and add them as chidren 
+          }
+        }
+    };
+
+    var rockTree = makeTree('rock');
+    var paperTree = makeTree('paper');
+    var scissorsTree = makeTree('scissors');
+
+    for ( var i = 1 ; i < rounds; i++ ) {
+      rockTree.addRound();
+
+      })
+    }   
+    rockTree.addRound();
+    rockTree.addRound();
+
+
+    
+
+
+
+
+
+
+    // at each round add to the current solution. 
+    collections.push(solution);
+  };
+  return collections;
 };
