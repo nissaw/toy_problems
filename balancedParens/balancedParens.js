13075cccb5250114c702d495dcc15837c78809a1
nissaw <nissa.wollum@gmail.com>
2015-11-05 09:38:30 -0800

    BalancedParens

--- balancedParens/balancedParens.js
+++ balancedParens/balancedParens.js
@@ -24,6 +24,43 @@
  *
  */
 var balancedParens = function(input){
+
+  var leftParen = 0;
+  var leftBrack = 0;
+  var leftArr = 0;
+
+  for (var i = 0 ; i < input.length; i++){
+    var char = input[i];
+
+    if (leftParen < 0 || leftBrack < 0 || leftArr < 0){
+      return false;
+    };
+    
+    if (char === '('){
+      leftParen++
+    } else if ( char === ')'){
+      leftParen--
+    } 
+
+    if (char === '{'){
+      leftBrack++
+    } else if ( char === '}'){
+      leftBrack--
+    }
+
+    if (char === '['){
+      leftArr++
+    } else if ( char === ']'){
+      leftArr--
+    } 
+  }
+
+if (leftParen === 0 && leftBrack === 0 && leftArr === 0){
+  return true;
+} else {
+  return false;
+}
+
 };
 
 
