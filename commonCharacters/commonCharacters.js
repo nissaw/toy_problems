0c6f7147f0165175bcb85a2121d474adf5eb41cc
nissaw <nissa.wollum@gmail.com>
2016-04-14 21:52:48 -0700

    Completes CommonCharacters

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -14,43 +14,11 @@
 // are Cap and lowercase === if so toLowerCase()
 
 var commonCharacters = function(string1, string2) {
-
- var resultArr = [];
- var letterObject = {};
-
- for (var i = 0; i < string1.length; i++) {
-  var letter = string1[i];
-  if (letter === " "){
-    continue;
-  } else {
-    letterObject[letter] = 1;
+  var common = [];
+  for(var i = 0; i < string1.length; i++){
+    if (string2.includes(string1[i]) && string1[i] !== ' ' && !common.includes(string1[i])){
+      common.push(string1[i])
+    }
   }
- }
-
-// var args = Array.slice.call(arguments) 
-// for arg[1] and above 
-for (var i = 0; i < string2.length; i++ ){
-  var newLetter = string2[i];
-  if (letterObject[newLetter]) {
-    letterObject[newLetter]++
-  }
-}
-
-for (var key in letterObject) {
-  if (letterObject[key] > 1){
-    resultArr.push(key);
-  }
-}
-
-return resultArr.join("");
-
+  return common.join('');
 };
-
-
-// commonCharacters('yellow', 'say you love me') >>> [y, e, l, o] >>> string >>> "yelo"
-
-// var objectify = function(str) {
-//   return str.split('').reduce(fuction(obj, char) {
-    
-//   }, {})
-// }
