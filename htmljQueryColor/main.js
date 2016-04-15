110a2d103a263b9f89be2e56324b2b96bb8ffe9c
nissaw <nissa.wollum@gmail.com>
2016-04-14 22:55:11 -0700

    Updates htmlQueryColor

--- htmljQueryColor/main.js
+++ htmljQueryColor/main.js
@@ -6,20 +6,28 @@ $(function(){
   // HINT: the `split` array method is your friend
 
   // TODO: your code here!
-var $bodyCopy = $('body');
-for (var i = 0; i < bodyCopy.children.length; i++){
-  var $p = bodyCopy.children()[i];
-  var wordArr = p.innerText.split(' ');
-    for (var j = 0; j < wordArr.length; j++ ){
-      wordArr[j].wrap('<span class="word"></span>')
-      // $('<span>' + wordArr[i] + '</span>').appendTo(p);
-      p.empty $.each(wordArr, = <span)
-    }
-}
+  var $pArr = $('body').find('p');
+  // jQuery each takes index then value
+  $pArr.each((i,$el) => {
+    var wordArr = $el.innerText.split(' ');
+    $el.innerText = '';
+    wordArr.forEach((word) => {
+      //this was surprising, $el is just a var pointing to a DOM element,
+      // not actually a DOM element, so it needs to be located with the $() selector
+      $($el).append('<span>' + word + '</span>')
+    })
+  })
 
   // --------------STEP 2--------------
   // Next, change spans to random colors, once per second
-add color attr to all spans set to function setInterval( random color generator, 1000)
+// add color attr to all spans set to function setInterval( random color generator, 1000)
   // TODO: your code here!
-
-});
+function getRandomColor(){
+  let color = '#' + Math.random().toString(16).slice(2,8)
+  console.log(color);
+  return color;
+}
+function setColor(){
+  $('body').find('span').each((i, el)=>{$(el).css('color', getRandomColor())});
+}
+setInterval(setColor, 1000);
